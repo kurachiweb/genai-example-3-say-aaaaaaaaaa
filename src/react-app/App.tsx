2e2e014bee89467faa9983ef072a79bb10a72b87
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2026-09-11 18:02:24 +0900

    chore: 作者へのリンクを追加

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -8,8 +8,18 @@ function App() {
 
 	return (
 		<div className="mx-auto flex min-h-dvh max-w-4xl flex-col gap-6 px-4 py-4">
-			<header>
+			<header className="flex flex-wrap justify-start items-baseline gap-x-1.5">
 				<h1 className="text-2xl font-bold">Say aaaaaaaaaa</h1>
+				<p className="text-xl">
+					by{" "}
+					<a
+						href="https://kurachiweb.com"
+						target="_blank"
+						className="hover:underline"
+					>
+						KurachiWeb
+					</a>
+				</p>
 			</header>
 
 			<MessageForm onPosted={refresh} />
